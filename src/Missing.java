413698ad0b11aea3d6503808d1097c405b082d9e
patrickmaishn <patricknmaina0@gmail.com>
2024-10-17 14:49:30 +0300

    Finding the missing number

---
+++ src/Missing.java
@@ -0,0 +1,25 @@
+/*
+* Given a list of integers except 1
+* 1st line input ⊃ int n
+* 2nd line input ⊃ n - 1
+* Each number is distinct btwn 1 & n
+* */
+
+import java.util.Scanner;
+public class Missing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        long totalSum = (long) n * (n + 1) / 2;
+
+        long sum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            sum += scanner.nextInt();
+        }
+
+        System.out.println(totalSum - sum);
+
+        scanner.close();
+    }
+}
