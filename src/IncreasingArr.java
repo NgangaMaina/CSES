9ea187ba5206a2142e470c3a25a029abd741c8b9
patrickmaishn <patricknmaina0@gmail.com>
2024-10-29 11:54:34 +0300

    Minimum moves for an increasing array

--- src/IncreasingArr.java
+++ src/IncreasingArr.java
@@ -7,32 +7,30 @@
 
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class IncreasingArr {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
 
-        int[] arr = new int[n];
+            int[] arr = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-
-        long moves = 0;
-
-        // Start from the second element and compare with the previous one
-        for (int i = 1; i < n; i++) {
-            if (arr[i] < arr[i - 1]) {
-                // Calculate how many moves are required to make arr[i] >= arr[i-1]
-                moves += (long) arr[i - 1] - arr[i];
-                arr[i] = arr[i - 1]; // Set arr[i] to arr[i - 1]
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
             }
-        }
 
-        System.out.println(moves);
-        scanner.close();
+            long moves = 0;
+
+            // Start from the second element and compare with the previous one
+            for (int i = 1; i < n; i++) {
+                if (arr[i] < arr[i - 1]) {
+                    // Calculate how many moves are required to make arr[i] >= arr[i-1]
+                    moves += (long) arr[i - 1] - arr[i];
+                    arr[i] = arr[i - 1]; // Set arr[i] to arr[i - 1]
+                }
+            }
+
+            System.out.println(moves);
+        }  // scanner will automatically close here
     }
 }
 
