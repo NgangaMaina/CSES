81fb3c1a0ff4dafcd001bf40d27dd2d0e0303acb
patrickmaishn <patricknmaina0@gmail.com>
2025-06-28 12:35:29 +0300

    Raab Game 1

--- src/RaabGame.java
+++ src/RaabGame.java
@@ -1,72 +1,58 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class RaabGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();  // Number of test cases
+        int t = sc.nextInt();
 
         while (t-- > 0) {
-            int n = sc.nextInt();  // Number of cards
-            int a = sc.nextInt();  // Player 1's score
-            int b = sc.nextInt();  // Player 2's score
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
             if (a + b > n) {
                 System.out.println("NO");
-                continue;
-            }
+            } else {
+                System.out.println("YES");
 
-            System.out.println("YES");
+                // Player A's sequence
+                int[] aCards = new int[n];
+                int[] bCards = new int[n];
 
-            int[] p1 = new int[n];
-            int[] p2 = new int[n];
-            boolean[] used = new boolean[n + 1]; // to avoid duplicates
-
-            int index = 0;
-            int high = n;
-            int low = 1;
-
-            // Player 1 wins 'a' rounds
-            for (int i = 0; i < a; i++) {
-                p1[index] = high;
-                p2[index] = low;
-                used[high] = true;
-                used[low] = true;
-                index++;
-                high--;
-                low++;
-            }
-
-            // Player 2 wins 'b' rounds
-            for (int i = 0; i < b; i++) {
-                p1[index] = low;
-                p2[index] = high;
-                used[high] = true;
-                used[low] = true;
-                index++;
-                high--;
-                low++;
-            }
-
-            // Fill remaining positions with unused equal cards
-            for (int i = 1; i <= n && index < n; i++) {
-                if (!used[i]) {
-                    p1[index] = i;
-                    p2[index] = i;
-                    index++;
+                // Fill A's and B's cards for A wins (first a turns)
+                for (int i = 0; i < a; i++) {
+                    aCards[i] = n - i; // A's highest cards
+                    bCards[i] = i + 1; // B's lowest cards
                 }
-            }
 
-            // Print player 1's card order
-            for (int i = 0; i < n; i++) {
-                System.out.print(p1[i] + " ");
-            }
-            System.out.println();
+                // Fill A's and B's cards for B wins (next b turns)
+                for (int i = 0; i < b; i++) {
+                    aCards[a + i] = a + i + 1; // A's next cards
+                    bCards[a + i] = n - b + i + 1; // B's highest remaining cards
+                }
 
-            // Print player 2's card order
-            for (int i = 0; i < n; i++) {
-                System.out.print(p2[i] + " ");
+                // Fill remaining cards for ties (last n - a - b turns)
+                int tieIndex = a + b;
+                for (int i = a + b; i < n; i++) {
+                    int val = tieIndex + 1;
+                    aCards[i] = val;
+                    bCards[i] = val;
+                    tieIndex++;
+                }
+
+                // Print A's sequence
+                for (int i = 0; i < n; i++) {
+                    System.out.print(aCards[i] + (i < n - 1 ? " " : ""));
+                }
+                System.out.println();
+
+                // Print B's sequence
+                for (int i = 0; i < n; i++) {
+                    System.out.print(bCards[i] + (i < n - 1 ? " " : ""));
+                }
+                System.out.println();
             }
-            System.out.println();
         }
+        sc.close();
     }
 }
